fc5695527581f96a809a2d89f7dbcb2a17579588
Md Sohanur Rahman Hridoy <sohanurrahmanhridoy007@gmail.com>
2022-12-01 03:26:35 +0600

    Done 2 tutorial fun projects...

---
+++ Fun Projects/01_clock.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from tkinter.ttk import*
+
+from time import strftime
+
+root =  Tk()
+root.title("DIGITAL CLOCK")
+
+
+def time():
+    string = strftime('%H:%M:%S %P')
+    label.config(text=string)
+    label.after(1000,time)
+
+label = Label(root,font = ("ds-digital",80),background="black",foreground="white")
+label.pack(anchor='center')
+time()
+
+mainloop()
