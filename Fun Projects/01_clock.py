787b60bae8354ac62c9a0caef5670b7241ab4b74
Md Sohanur Rahman Hridoy <sohanurrahmanhridoy007@gmail.com>
2022-12-03 14:42:35 +0600

    Completed Day-05 Challenge..

--- Fun Projects/01_clock.py
+++ Fun Projects/01_clock.py
@@ -12,7 +12,7 @@ def time():
     label.config(text=string)
     label.after(1000,time)
 
-label = Label(root,font = ("ds-digital",80),background="black",foreground="white")
+label = Label(root,font = ("ds-digital",80),background="black",foreground="cyan")
 label.pack(anchor='center')
 time()
 
