4c5a58059ef9d9e693fa4c95c342f4220be4d65a
Md Sohanur Rahman Hridoy <sohanurrahmanhridoy007@gmail.com>
2022-12-04 18:11:24 +0600

    Completed Day - 07 --> challenge...

---
+++ 100 Days Of Code/Day 7 - Exercise 1: Calculator using Python/01_main.py
@@ -0,0 +1,7 @@
+print(5 + 6)
+print(15 - 6)
+print(15 * 6)
+print(15 / 6)
+print(15 // 6)#Floor division
+print(15 % 6)
+print(5 ** 3)#Exponential
---
+++ 100 Days Of Code/Day 7 - Exercise 1: Calculator using Python/02_calculator.py
@@ -0,0 +1,10 @@
+#Exercise 1: Calculator using Python
+
+a = 105
+b = 11
+print("a = ", a)
+print("b = ", b)
+print("Addition of a and b is (a + b) = ",a + b)
+print("Substraction of a and b is (a - b) = ",a - b)
+print("Multiplication of a and b is (a * b) = ",a * b)
+print("Division of a and b is (a / b) = ",a / b)
