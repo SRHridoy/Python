d13ab12e56ce3ab56aa4908a30ac3b8518a8e001
Md Sohanur Rahman Hridoy <sohanurrahmanhridoy007@gmail.com>
2022-12-07 01:46:51 +0600

    Learnt some about TKinter...

---
+++ TKinter/02_Positioning With Tkinter's Grid System .py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+
+lb1= Label(root,text="Today, I am learing Grid...").grid(row=0,column=0)
+lb2= Label(root,text="I found my interest in Python!").grid(row=1,column=5)
+# lb3= Label(root,text="Who are you?")
+
+# lb1.grid(row=0,column=0)
+# lb2.grid(row=1,column=5)
+# lb3.grid(row=1,column=1)
+
+root.mainloop()
