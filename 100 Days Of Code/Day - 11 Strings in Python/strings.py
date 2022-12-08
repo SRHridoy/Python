1ae9ad50eff5a715b852248b364a0abb552d3839
Md Sohanur Rahman Hridoy <sohanurrahmanhridoy007@gmail.com>
2022-12-08 22:08:19 +0600

    Completed Day -11 Challenge...

---
+++ 100 Days Of Code/Day - 11 Strings in Python/strings.py
@@ -0,0 +1,40 @@
+name = "Hridoy"
+friend = "Ozaer"
+anotherFriend = 'Nova'
+#apple = "He said, \"I want to eat a apple\""
+#or
+apple = 'He said, "I want a chocolate"'
+print("Hello, " + name)
+print(apple)
+
+#Mulitiline strings-->
+conversation = '''He said,
+Hi Hri
+Hey What's up?
+"I am doing this"
+'''
+#used to print everything in it..
+all_str = """
+Example
+name = "Harry"
+print("Hello, " + name)
+Output
+Hello, Harry
+print('He said, "I want to eat an apple".')
+"""
+print(conversation)
+print(all_str)
+
+
+
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+# print(name[5])
+#print(name[6]) -->throws an error
+
+#let's use for loop-->
+for character in all_str:
+    print(character)
