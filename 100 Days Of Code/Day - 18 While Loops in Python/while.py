7175cec8664a3c50de58a0c77c2831c8b00a087f
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2022-12-16 00:15:59 +0600

    Completed Day#18 Challenge...

---
+++ 100 Days Of Code/Day - 18 While Loops in Python/while.py
@@ -0,0 +1,13 @@
+""" i = int(input("Enter the number: "))
+while(i<=38):
+    i = int(input("Enter the number: "))
+    print(i)
+print("Done with the loop") """
+
+count = 5
+while (count > 0):
+    print(count)
+    count = count - 1
+else:
+    print("I am inside else")
+
