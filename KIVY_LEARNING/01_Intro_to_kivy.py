4a5295802d632a8c956bf18b5f624895b6bffbc6
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2023-01-23 00:45:36 +0600

    Completed DAY-55 Challenge...

---
+++ KIVY_LEARNING/01_Intro_to_kivy.py
@@ -0,0 +1,12 @@
+##############################KIVY WITH PYTHON##############################
+
+import kivy 
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    
+    def build(self):
+        return Label(text = 'Hello World')
+if __name__ == '__main__':
+    MyApp().run()
